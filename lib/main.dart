983d2badb4b85214ff3f08110ad5f6fa07946f46
Gali998 <galimannage6.gm@gmail.com>
2021-09-19 07:13:12 +0530

    welcome screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../screens/HomePage.dart';
+import '../screens/welcomeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: WelcomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
